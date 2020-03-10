d1a5d95d2d17908f6b308c55e3abfb1a6cef4ed9
slow126 <spencerlow126@me.com>
2020-03-10 15:19:49 -0600

    Task 1 and 2

--- task1.py
+++ task1.py
@@ -6,8 +6,9 @@ PATH = "calibration_img"
 files = os.listdir(PATH)
 
 def find_corners(in_img, pattern=(10, 7)):
-
-    color_img = cv2.cvtColor(in_img, cv2.COLOR_GRAY2RGB)
+    color_img = in_img
+    in_img = cv2.cvtColor(in_img, cv2.COLOR_RGB2GRAY)
+    # color_img = cv2.cvtColor(in_img, cv2.COLOR_GRAY2RGB)
     # gray_gpu = cv2.UMat(gray)
     # pattern = (10, 7)
     pattern2 = (23, 11)
@@ -33,6 +34,7 @@ def find_corners(in_img, pattern=(10, 7)):
     # cv2.imshow("img", annotated_gray)
     # cv2.waitKey()
     # cv2.destroyAllWindows()
+    objpoints = np.array(objpoints) * 3.88
     return annotated_gray, sub_corners, objpoints, imgpoints
 
 for file in files:
@@ -44,6 +46,42 @@ for file in files:
         right = img
         annotated_right, right_sub, right_obj, right_img = find_corners(right)
 
+R1 = np.load("R_left.npy")
+R2 = np.load("R_right.npy")
+P1 = np.load("P_left.npy")
+P2 = np.load("P_right.npy")
+Q = np.load("Q.npy")
+left_mtx = np.load("left-mtx.npy")
+left_dist = np.load("left-dist.npy")
+right_mtx = np.load("right-mtx.npy")
+right_dist = np.load("right-dist.npy")
+
+left_img_corners = [left_sub[0], left_sub[9], left_sub[-1], left_sub[-10]]
+left_obj_corners = [left_obj[0][0], left_obj[0][9], left_obj[0][-1], left_obj[0][-10]]
+right_img_corners = [right_sub[0], right_sub[9], right_sub[-1], right_sub[-10]]
+right_obj_corners = [right_obj[0][0], right_obj[0][9], right_obj[0][-1], right_obj[0][-10]]
+
+left_rect = cv2.undistortPoints(np.array(left_img_corners), left_mtx, left_dist, np.array(left_obj_corners), R1, P1)
+right_rect = cv2.undistortPoints(np.array(right_img_corners), right_mtx, right_dist, np.array(right_obj_corners), R2, P2)
+
+t_left = np.squeeze(left_rect)
+t2 = [[0],[0],[0],[0]]
+diff = np.squeeze(left_rect - right_rect)
+disparity = diff[:,0]
+
+# t = np.dstack((t, disparity))
+
+
+t_left = np.dstack((left_rect, t2))
+t_right = np.dstack((right_rect, t2))
+# t = np.append(t, np.array([[disparity]]).transpose(), axis=2)
+for i in range(4):
+    t_left[i,0,2] = disparity[i]
+    t_right[i,0,2] = disparity[i]
+
+left_persp = cv2.perspectiveTransform(t_left, Q)
+right_persp = cv2.perspectiveTransform(t_right, Q)
+x = 1
 
 
 
--- task2.py
+++ task2.py
@@ -1,20 +1,114 @@
 import cv2
 import numpy as np
+import os
+
+PATH = "pitch_img/"
+
+
+prev_img = np.zeros((480, 640, 3))
+base_imgL = cv2.imread(os.path.join(PATH, 'left/L_00001.jpeg'))
+left_list = os.listdir(os.path.join(PATH, 'left'))
+left_list.sort()
+base_imgR = cv2.imread(os.path.join(PATH, 'right/R_00001.jpeg'))
+right_list = os.listdir(os.path.join(PATH, 'right'))
+right_list.sort()
+
+params = cv2.SimpleBlobDetector_Params()
+# Change thresholds
+params.minThreshold = 0
+params.maxThreshold = 200
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 120
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.4
+
+# Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.2
+
+# Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.1
+detector = cv2.SimpleBlobDetector_create(params)
+
+prev_imgL = base_imgL
+prev_imgR = base_imgR
+prev_keyL = []
+prev_keyR = []
+
+
+for i in range(left_list.__sizeof__()):
+    imgL = cv2.imread(os.path.join(PATH, "left", left_list[i]))
+    imgR = cv2.imread(os.path.join(PATH, "right", right_list[i]))
+
+    # if prev_keyL != []:
+    #     for i in range(len(prev_keyL)):
+    #         cv2.circle(prev_imgL, (int(prev_keyL[i].pt[0]), int(prev_keyL[i].pt[1])), int(prev_keyL[i].size / 2), (255,255,255), thickness=-1)
+    #
+    # cv2.imshow("prev", prev_imgL)
+    # cv2.waitKey()
+
+    diffL = cv2.absdiff(prev_imgL, imgL)
+    diffR = cv2.absdiff(prev_imgR, imgR)
+
+    diffL[diffL < 10] = 0
+    diffL[diffL >= 10] = 150
+    diffR[diffR < 10] = 0
+    diffR[diffR >= 10] = 150
+
+    # if prev_keyL != []:
+    #     for i in range(len(prev_keyL)):
+    #         cv2.circle(diffL, (int(prev_keyL[i].pt[0]), int(prev_keyL[i].pt[1])), int(prev_keyL[i].size / 2), (255,255,255), thickness=-1)
 
 
 
+    diffL = ~diffL
+    diffR = ~diffR
+    # cv2.imshow("output", diff)
+    # cv2.waitKey(0)
+    diffL = cv2.erode(cv2.dilate(diffL, (6,6)),(6,6))
+    diffR = cv2.erode(cv2.dilate(diffR, (6,6)),(6,6))
+
+    keypoints = detector.detect(diffL)
+    prev_keyL = keypoints
+    if len(keypoints) >= 0:
+        im_with_keypointsL = cv2.drawKeypoints(imgL, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    else:
+        im_with_keypointsL = cv2.drawKeypoints(imgL, keypoints[1], np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 
-vc_left = cv2.VideoCapture("BaseBall_Pitch_R.avi")
-if vc_left.isOpened():
-    rval, im = vc_left.read()
-else:
-    rval = False
+    keypoints = detector.detect(diffR)
+    prev_keyR = keypoints
+    if len(keypoints) >= 0:
+        im_with_keypointsR = cv2.drawKeypoints(imgR, keypoints, np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    else:
+        im_with_keypointsR = cv2.drawKeypoints(imgR, keypoints[1], np.array([]), (0, 0, 255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # cv2.imwrite("Annotated_Images/" + file, im_with_keypoints)
 
-counter = 0;
+    prev_imgL = imgL
+    prev_imgR = imgR
 
-while rval:
-    rval, im_L = vc_left.read()
-    cv2.imshow("left", im_L)
-    cv2.waitKey()
+    cv2.imshow("outputL", im_with_keypointsL)
+    cv2.imshow("outputR", im_with_keypointsR)
+    cv2.waitKey(50)
 
+
+
+# count = 0
+# vc_left = cv2.VideoCapture("BaseBall_Pitch_L.avi")
+# if vc_left.isOpened():
+#     rval, im = vc_left.read()
+# else:
+#     rval = False
+#
+# counter = 0;
+#
+# while rval:
+#     count = count + 1
+#     rval, im_L = vc_left.read()
+#     cv2.imwrite(os.path.join("pitch_img/left", "L_" + str(count).zfill(5) + ".jpeg"), im_L)
+#
